65e0c5fd6aaeb29ab697f8c314f12e3fcb662f30
Assem Saeed <saeedassem1637@gmail.com>
2022-09-25 23:36:46 +0200

    update

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDom.createRoot(document.querySelector("#root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
